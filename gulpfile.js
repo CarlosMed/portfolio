9f82fc773a007d972249de315bdfe21c07b184a3
carlos medina <Carlos@CM-RMP.local>
2015-08-29 23:49:28 -0400

    Added AutoPrefixer

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
+var autoprefixer = require('gulp-autoprefixer');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -15,6 +16,7 @@ gulp.task('serve', ['sass'], function() {
 
     gulp.watch("./scss/*.scss", ['sass']);
     gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch('./scss/main.scss', ['autoprefixer']);
 });
 
 gulp.task('plumber',['sass'], function() {
@@ -30,6 +32,7 @@ gulp.task('plumber',['sass'], function() {
 gulp.task('sass', function() {
     return gulp.src("./scss/*.scss")
         .pipe(sass())
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(gulp.dest("./css"))
         .pipe(browserSync.stream());
 });
