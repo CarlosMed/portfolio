fed8dd4a508572c606a18e31399770088bfaaa5d
Carlos Medina <carlos.w.med@gmail.com>
2018-11-09 08:14:46 -0500

    refactor: add all of the index and sccs file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <main>
+        <header>
+          <div className="img-container">
+            <img src="https://source.unsplash.com/random" alt="" />
+            <div className="grid">
+              <div className="item" />
+              <div className="item" />
+              <div className="item" />
+              <div className="item" />
+              <div className="item" />
+              <div className="item" />
+            </div>
+          </div>
+          <nav>
+            <ul>
+              <li>About</li>
+              <li>Email</li>
+              <li>Codepen</li>
+            </ul>
+          </nav>
         </header>
-      </div>
+
+        <section className="about">
+          <h1>Carlos Medina</h1>
+          <h2>Software Engineer & Designer</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+            ipsum, dolor sit amet consectetur adipisicing elit. Accusantium,
+            laudantium.
+          </p>
+        </section>
+
+        <div className="line-container">
+          <div className="line" />
+        </div>
+
+        <section className="jobs">
+          <div className="job">
+            <p>Saf Technologies</p>
+            <span>+</span>
+          </div>
+          <div className="job">
+            <p>The Trailer Store</p>
+            <span>+</span>
+          </div>
+          <div className="job">
+            <p>Life Made a Little Easier</p>
+            <span>+</span>
+          </div>
+          <div className="job">
+            <p>ToDiddlyDo</p>
+            <span>+</span>
+          </div>
+          <div className="job">
+            <p>More to Come</p>
+            <span>+</span>
+          </div>
+          <div className="job">
+            <p>name06</p>
+            <span>+</span>
+          </div>
+        </section>
+      </main>
     );
   }
 }
