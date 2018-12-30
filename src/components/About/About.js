9888c70c8a8cfb37a1d3a2e1d143a19f562533a3
Carlos Medina <carlos.w.med@gmail.com>
2018-12-30 03:05:30 -0500

    refactor: description to better describe me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,8 +6,13 @@ export function About() {
       <h1>Carlos Medina</h1>
       <h2>Software Engineer & Designer</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum,
-        dolor sit amet consectetur adipisicing elit. Accusantium, laudantium.
+        I’m a Frontend designer based in Charlotte currently in freelancing and
+        learning new concepts and tools. Starting off with a focus on technology
+        and art, I followed my passion to enrich my skillsets and then dabbled
+        into somewhere between web technology, screen art and interface
+        animations by self-motivated learning. Throughout my career as an
+        independent creator, I wear different hats to bring digital experiences
+        to life across a variety of design spectrums.
       </p>
     </section>
   );
