5ede47dfb8c966d61c9fa759db194fa0f2cfa236
Carlos Medina <carlos.w.med@gmail.com>
2019-01-08 00:55:50 -0500

    refactor: to show home as the main content

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,14 +4,14 @@ import { Header } from './Header/Header';
 import { About } from './About/About';
 
 const Home = props => {
-  return <>
+  return <div className="main">
         <Header />
         <About />
         <div className="line-container">
           <div className="line" />
         </div>
         <Work />
-      </>;
+      </div>;
 };
 
 export default Home;
