76101d02caea9c62ed5f1538d6080023fc0d72f8
Carlos Medina <carlos.w.med@gmail.com>
2019-01-22 13:10:51 -0500

    feature: react-spring animations

--- src/components/Description/JobDescription.js
+++ src/components/Description/JobDescription.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Spring, animated } from 'react-spring';
+
 import { JobDescriptionNav } from './JobDescriptionNav';
-import { randomColor } from '../../helpers/helpers';
 
 const JobDescription = ({
   jobInfo,
@@ -11,53 +12,66 @@ const JobDescription = ({
   ...props
 }) => {
   const { description, title, workImage, technologies, view, github } = jobInfo;
-  const styles = { backgroundColor: randomColor() };
 
   return (
     <>
-      <img
-        src={workImage !== undefined ? workImage.fields.file.url : ''}
-        alt=""
-        style={styles}
-      />
-      <div className="left">
-        <JobDescriptionNav
-          handlePrev={handlePrev}
-          handleNext={handleNext}
-          jobLength={jobLength}
-          currentJobNumber={currentJobNumber}
-        />
-        <div className="title">
-          <h2>{title}</h2>
-          <div>
-            {view ? (
-              <a href={view} target="_blank" rel="noopener noreferrer">
-                View
-              </a>
-            ) : (
-              ''
-            )}
-            {github ? (
-              <a href={github} target="_blank" rel="noopener noreferrer">
-                Github
-              </a>
-            ) : (
-              ''
-            )}
-          </div>
-        </div>
-        <ul>
-          {technologies.map((technology, i) => (
-            <li key={i}>
-              <i
-                className={`devicon-${technology.toLowerCase()}-plain colored`}
-              />
-              <span>{technology}</span>
-            </li>
-          ))}
-        </ul>
-        <p>{description}</p>
-      </div>
+      <Spring
+        native
+        from={{ opacity: 0, transform: 'translate3d(-100%, 0, 0)' }}
+        to={{ opacity: 1, transform: 'translate3d(0, 0, 0)' }}>
+        {props => (
+          <animated.img
+            src={workImage !== undefined ? workImage.fields.file.url : ''}
+            alt=""
+            style={props}
+          />
+        )}
+      </Spring>
+      <Spring
+        native
+        from={{ opacity: 0 }}
+        to={{ opacity: 1 }}>
+        {props => (
+          <animated.div className="left" style={props}>
+            <JobDescriptionNav
+              handlePrev={handlePrev}
+              handleNext={handleNext}
+              jobLength={jobLength}
+              currentJobNumber={currentJobNumber}
+            />
+            <div className="title">
+              <h2>{title}</h2>
+              <div>
+                {view ? (
+                  <a href={view} target="_blank" rel="noopener noreferrer">
+                    View
+                  </a>
+                ) : (
+                  ''
+                )}
+                {github ? (
+                  <a href={github} target="_blank" rel="noopener noreferrer">
+                    Github
+                  </a>
+                ) : (
+                  ''
+                )}
+              </div>
+            </div>
+            <ul>
+              {technologies.map((technology, i) => (
+                <li key={i}>
+                  <i
+                    className={`devicon-${technology.toLowerCase()}-plain colored`}
+                  />
+                  <span>{technology}</span>
+                </li>
+              ))}
+            </ul>
+            <p>{description}</p>
+          </animated.div>
+        )}
+      </Spring>
     </>
   );
 };
--- src/components/Home/Work/Jobs.js
+++ src/components/Home/Work/Jobs.js
@@ -1,8 +1,12 @@
-import { Job } from './Job';
 import React, { Component } from 'react';
+import { Trail, animated } from 'react-spring';
+
+import { Job } from './Job';
+
 import { client } from '../../../helpers/helpers';
 
-class Jobs extends Component { constructor(props) {
+class Jobs extends Component {
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -22,8 +26,22 @@ class Jobs extends Component { constructor(props) {
     const { jobs } = this.state;
     return (
       <section className="jobs">
-        {jobs.length > 0 &&
-          jobs.map((job, i) => <Job jobs={job.fields} key={i} />)}
+        {jobs.length > 0 && (
+          <Trail
+            native
+            keys={job => job.sys.id}
+            items={jobs}
+            from={{ opacity: 0, transform: 'translate3d(100%, 0, 0)' }}
+            to={{ opacity: 1, transform: 'translate3d(0, 0, 0)' }}>
+            {(job, i) => props => {
+              return (
+                <animated.div className="job" style={props} key={job.sys.id}>
+                  <Job jobs={job.fields} />
+                </animated.div>
+              );
+            }}
+          </Trail>
+        )}
       </section>
     );
   }
