4379f9f0b6350001829b41e5990a6ec004764fe2
Carlos Medina <carlos.w.med@gmail.com>
2024-08-21 16:13:10 -0400

    feat: exported the about to its own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,16 @@ import Header from "@/components/Header";
 import Section from "@/components/Section";
 import Card from "@/components/Card";
 import { WORK_LIST } from "@/lib/constants";
+import About from "@/components/about/About";
 
 export default function Home() {
   return (
     <>
       <Header />
       <main className="w-full space-y-5 px-4 lg:space-y-44 lg:px-10">
-        <Section title={"Who"} anchorName="about">
-          <div className="mt-2 space-y-4 text-sm md:mt-5 md:max-w-96">
-            <p>
-              A passionate fullstack senior engineer at Method (not the soap
-              company), a boutique consultancy agency specializing in innovative
-              tech solutions.
-            </p>
-            <p>
-              With a keen eye for detail and an eye for frontend development, I
-              thrive in crafting seamless user experiences while architecting
-              robust backend systems.
-            </p>
-            <p>
-              I&apos;m constantly exploring new technologies and seeking to push
-              the boundaries of what&apos;s possible in the digital landscape. I
-              have had the great pleasure in working on a diverse background
-              collaboration with top industry leaders.
-            </p>
-          </div>
-        </Section>
+          <Section title={"Who"} anchorName="about">
+            <About />
+          </Section>
         <Section title="Why" className="space-y-8" anchorName="work">
           <div className="space-y-8">
             {WORK_LIST.map(({ id, name, year }) => (
---
+++ src/components/about/About.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="mt-2 space-y-4 text-sm md:mt-5 md:max-w-96">
+      <p>
+        A passionate fullstack senior engineer at Method (not the soap company),
+        a boutique consultancy agency specializing in innovative tech solutions.
+      </p>
+      <p>
+        With a keen eye for detail and an eye for frontend development, I thrive
+        in crafting seamless user experiences while architecting robust backend
+        systems.
+      </p>
+      <p>
+        I&apos;m constantly exploring new technologies and seeking to push the
+        boundaries of what&apos;s possible in the digital landscape. I have had
+        the great pleasure in working on a diverse background collaboration with
+        top industry leaders.
+      </p>
+    </div>
+  );
+}
