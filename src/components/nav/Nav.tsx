7bca58343af0dfb29510261287e2cf226211008f
Carlos Medina <carlos.w.med@gmail.com>
2024-08-23 19:34:01 -0400

    fix: nav z-index

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -4,7 +4,7 @@ import { NAV_LIST } from "@/lib/constants";
 export default function Nav() {
   return (
     <>
-      <nav className="fixed bottom-8 mx-auto flex h-16 w-11/12 max-w-2xl items-center justify-between rounded-md bg-darkGreen px-5 text-white">
+      <nav className="fixed bottom-8 z-10 mx-auto flex h-16 w-11/12 max-w-2xl items-center justify-between rounded-md bg-darkGreen px-5 text-white">
         <div className="space-x-12">
           {NAV_LIST.map(({ name, href }) => (
             <a href={href} key={name}>
