a3b89f25e650b71f2a6a3a1528bf54d87d401bfd
Carlos Medina <carlos.w.med@gmail.com>
2024-08-23 19:33:02 -0400

    fix: footer font clamp

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="">
-      <p className="text-[clamp(2.85rem,30vw,5rem)] font-bold text-tan">
+      <p className="text-[clamp(2.75rem,-0.125rem+24.075vw,37rem)] font-bold leading-none text-tan">
         Lets Connect
       </p>
     </footer>
