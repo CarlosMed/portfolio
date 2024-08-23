619279d2c3dd259cf9df539cd3ca64d41fb4e64b
Carlos Medina <carlos.w.med@gmail.com>
2024-08-23 18:21:29 -0400

    feat: resume button

--- src/components/soon/Soon.tsx
+++ src/components/soon/Soon.tsx
@@ -1,45 +1,69 @@
-'use client'
+"use client";
 import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
+import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa6";
 
 export default function Soon() {
-  const mainRef = useRef(null)
+  const mainRef = useRef(null);
 
   useEffect(() => {
     const ctx = gsap.context(() => {
-      gsap.timeline()
-        .from('main > p', {
+      gsap
+        .timeline()
+        .from("main > p, a", {
           autoAlpha: 0,
           scaleY: 1,
           duration: 2,
           stagger: 3,
-          ease: 'power2.inOut',
+          ease: "power2.inOut",
         })
-        .from('div > p', {
-          autoAlpha: 0,
-          scaleY: 1,
-          duration: 1,
-          stagger: +0.3,
-          ease: 'power2.inOut',
-        }, '<')
-    }, mainRef)
+        .from(
+          "div > p",
+          {
+            autoAlpha: 0,
+            scaleY: 1,
+            duration: 1,
+            stagger: +0.3,
+            ease: "power2.inOut",
+          },
+          "<",
+        );
+    }, mainRef);
 
     return () => {
-      ctx.revert()
-    }
-  }, [])
+      ctx.revert();
+    };
+  }, []);
 
-  return (<>
-    <main ref={mainRef} className="flex min-h-[100dvh] select-none flex-col items-center justify-between overflow-hidden px-5 py-1 text-[4em] font-bold md:p-10">
-      <div className='name flex w-full items-center justify-between font-khand font-bold tracking-tight'>
-        <p>Carlos</p>
-        <p>Medina</p>
-      </div>
-      <p className='text-2xl font-bold uppercase tracking-[0.75rem]'>Website</p>
-      <div className='flex w-full items-center justify-between font-khand font-bold tracking-tight'>
-        <p>Coming</p>
-        <p>Soon</p>
-      </div>
-    </main>
-  </>)
+  return (
+    <>
+      <main
+        ref={mainRef}
+        className="flex min-h-[100dvh] select-none flex-col items-center justify-between overflow-hidden px-5 py-1 text-[4em] font-bold md:p-10"
+      >
+        <div className="name flex w-full items-center justify-between font-khand font-bold tracking-tight">
+          <p>Carlos</p>
+          <p>Medina</p>
+        </div>
+        <div className="flex flex-col items-center space-y-4">
+          <p className="pl-3 text-2xl font-bold uppercase tracking-[0.75rem]">
+            Website
+          </p>
+          <Link
+            href="Carlos_Medina_Resume.pdf"
+            target="_blank"
+            className="flex items-center justify-center space-x-2 rounded-lg bg-darkGreen px-4 py-3 text-xs tracking-wide text-white/90 hover:space-x-3 hover:bg-darkGreen/90 hover:text-white/100"
+          >
+            <span>View My Resume</span>
+            <FaArrowRight />
+          </Link>
+        </div>
+        <div className="flex w-full items-center justify-between font-khand font-bold tracking-tight">
+          <p>Coming</p>
+          <p>Soon</p>
+        </div>
+      </main>
+    </>
+  );
 }
