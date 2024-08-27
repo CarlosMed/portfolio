f2cd68980b59088c5703994755ad7bf8477aa8c0
Carlos Medina <carlos.w.med@gmail.com>
2024-08-27 17:34:51 -0400

    fix: footer size reduction to avoid scrollbars

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="">
-      <p className="text-[clamp(2.75rem,-0.125rem+24.075vw,37rem)] font-bold leading-none text-tan">
+    <footer>
+      <p className="text-[clamp(2.75rem,-0.125rem+23.075vw,37rem)] font-bold leading-none text-tan">
         Lets Connect
       </p>
     </footer>
