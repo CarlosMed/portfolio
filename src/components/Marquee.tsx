a49a2fb60c5ad9dac0c9945168a01898ddb36b08
Carlos Medina <carlos.w.med@gmail.com>
2024-08-27 11:05:20 -0400

    fix: marquee stuttering

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -3,20 +3,18 @@ import { motion } from "framer-motion";
 
 export default function Marquee({ children, duration }) {
   return (
-    <div className="flex ">
-      <motion.div
-        className="flex "
-        animate={{ x: `-50%` }}
-        transition={{
-          duration,
-          ease: "linear",
-          repeat: Infinity,
-          repeatType: "loop",
-        }}
-      >
-        <div className="marquee">{children}</div>
-        <div className="marquee">{children}</div>
-      </motion.div>
-    </div>
+    <motion.div
+      className="flex w-fit"
+      animate={{ x: `-50%` }}
+      transition={{
+        duration,
+        ease: "linear",
+        repeat: Infinity,
+        repeatType: "loop",
+      }}
+    >
+      <div className="marquee">{children}</div>
+      <div className="marquee">{children}</div>
+    </motion.div>
   );
 }
