343035d48f2162de620867e877194fdbaa44d04f
Carlos Medina <carlos.w.med@gmail.com>
2024-08-28 23:30:02 -0400

    fix: about me wording

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -2,15 +2,11 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="mt-2 space-y-4 text-sm md:mt-5 md:max-w-96">
+    <>
       <p>
-        A passionate fullstack senior engineer at Method (not the soap company),
-        a boutique consultancy agency specializing in innovative tech solutions.
-      </p>
-      <p>
-        With a keen eye for detail and an eye for frontend development, I thrive
-        in crafting seamless user experiences while architecting robust backend
-        systems.
+        A passionate fullstack senior engineer with a keen eye for detail and an
+        eye for frontend development, I thrive in crafting seamless user
+        experiences while architecting robust backend systems.
       </p>
       <p>
         I&apos;m constantly exploring new technologies and seeking to push the
@@ -18,6 +14,6 @@ export default function About() {
         the great pleasure in working on a diverse background collaboration with
         top industry leaders.
       </p>
-    </div>
+    </>
   );
 }
